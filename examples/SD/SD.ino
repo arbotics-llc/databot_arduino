8d016b67a71d2669954e7e922c003643fb18802d
Sean Tucker <sean.michael.tucker@gmail.com>
2019-06-17 01:50:47 -0600

    prototype interface for selecting files from sd card, still need to add different directories and a way to build the filesystem on each boot and select which directory to write to / figure out good names to give files

--- examples/SD/SD.ino
+++ examples/SD/SD.ino
@@ -2,9 +2,10 @@
 
 OpenLog myLog; //Create instance
 
+long totalSize = 0;
+
 void setup()
 {
-  long totalSize = 0;
   Wire.begin();
   Wire.setClock(400000); //Go super fast
   myLog.begin(); //Open connection to OpenLog (no pun intended)
@@ -16,13 +17,12 @@ void setup()
   //Record something to the default log
   myLog.println("OpenLog Read File Test");
 
-  String myFile = "longFile.txt";
+  String myFile = "imusensoz.txt";
 
   myLog.removeFile(myFile);
 
   //Get size of file
   int32_t sizeOfFile = myLog.size(myFile);
-
   if (sizeOfFile == -1) //File does not exist. Create it.
   {
     Serial.println(F("File not found, creating one..."));
@@ -78,42 +78,126 @@ void setup()
   Serial.println(F("Done!"));
 
   //first get rid of all those LOG****.TXT files that get generated from somewhere
-  long startTime = millis();
   Serial.print("Removed files:");
   Serial.println(myLog.removeFile("LOG*.TXT"));
-  myLog.searchDirectory("*");
+
   //we are going to abuse String because we don't have std or stl
   //note: calling any OpenLog member function inbetween searchDirectory() and
   //getNextdirectory() causes problems, this why we build our jank list
   String fileList = "";
-  for(String currFile = myLog.getNextDirectoryItem(); currFile != "";currFile = myLog.getNextDirectoryItem()){
-    //Serial.println(currFile);
-    fileList += currFile;
-    fileList += '\n';
-    //Serial.println(myLog.size(currFile));
+  int listIndex = 0;
+  #define filesperpage 10
+  const int numFiles = createFileList("*", fileList, myLog);
+  printFileList(fileList, 100, 0);
+
+  moreFile:
+  if(listIndex < 0){
+    listIndex = 0;
   }
+  Serial.print("list index is");
+  Serial.println(listIndex);
+  listIndex = printFileList(fileList, filesperpage, listIndex);
+  int filesLeft = numFiles - listIndex;
+
+  Serial.print("you have files left:");
+  Serial.println(filesLeft);
+
+  Serial.print("Select a file 0 - ");
+  Serial.print(filesperpage-1);
+  if(filesLeft){
+    Serial.print(", more files available, to see more press n");
+    if(listIndex > filesperpage){
+      Serial.print(", to go back press b");
+    }
+    Serial.println("");
+  }else{
+    Serial.println("end of file list, press b to go back");
+  }
+
+  while(!Serial.available()){
+  }
+  uint8_t userinput = Serial.read();
+  while(Serial.available()){
+    Serial.read();
+  }
+  if(userinput == 'n'){
+     goto moreFile;
+  }
+  if(userinput == 'b'){
+     listIndex = listIndex - filesperpage - (filesperpage % listIndex);
+     goto moreFile;
+  }
+  userinput -= 48;
+  Serial.print("you selected file: ");
+  Serial.println(userinput);
+
   String currFile = "";
+  int j = 0;
   for(int i = 0; i < fileList.length(); i++){
     char c = fileList[i];
     if(c != '\n'){
       currFile += c;
     }else{
+      j++;
+      if(j == userinput){
+      Serial.print("file ");
+      Serial.print(j);
+      Serial.print(": ");
       Serial.println(currFile);
       Serial.print("  Size:");
       sizeOfFile = myLog.size(currFile);
       totalSize += sizeOfFile;
       Serial.println(sizeOfFile);
       myLog.readContinuous(sizeOfFile, currFile, i2cBuffHandler);
+      }
       currFile = "";
     }
   }
-  long endTime = millis();
-  Serial.println("");
-  Serial.print("took this long to read erryting: ");
-  Serial.println(endTime - startTime);
-  Serial.print("and errything was this many bytes: ");
-  Serial.println(totalSize);
-  
+}
+
+int createFileList(String filter, String &list, OpenLog &myLog){
+  int numFiles = 0;
+
+  myLog.searchDirectory(filter);
+
+  for(String currFile = myLog.getNextDirectoryItem(); currFile != "";currFile = myLog.getNextDirectoryItem()){
+    list += currFile;
+    list += '\n';
+    numFiles++;
+  }
+
+  return numFiles;
+}
+
+int printFileList(String &list, int filesPerPage, int startIndex){
+ String currFile = "";
+ int listIndex = 0;
+ int numPrinted = 0;
+ int32_t sizeOfFile = 0;
+
+ for(int i = 0; i < list.length(); i++){
+   char c = list[i];
+   if(c != '\n'){
+     currFile += c;
+   }else{
+     if(listIndex >= startIndex){
+       numPrinted++;
+       Serial.print(listIndex - startIndex);
+       Serial.print(": ");
+       Serial.println(currFile);
+       Serial.print("  Size:");
+       sizeOfFile = myLog.size(currFile);
+       totalSize += sizeOfFile;
+       Serial.println(sizeOfFile);
+     }
+     currFile = "";
+     listIndex++;
+     if(numPrinted == filesPerPage){
+       break;
+     }
+   }
+ }
+ return listIndex;
 }
 
 void i2cBuffHandler(uint16_t buffSize,uint8_t *data){
