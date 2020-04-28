39a180456b2dcd9ad0c4c7824c8fc0887d077e05
Brian Dennis <pzinbox@gmail.com>
2020-04-28 09:42:19 -0700

    device name prefix is DB_

--- examples/PhyPhox/cube_set_name/cube_set_name.ino
+++ examples/PhyPhox/cube_set_name/cube_set_name.ino
@@ -1,10 +1,20 @@
-String NAMED = String("Yellow");
+String NAMED = String("databot");
+//String NAMED = String("Midnight");
+//String NAMED = String("Grape");
+//String NAMED = String("Royal");
+//String NAMED = String("Teal");
+//String NAMED = String("Plum");
+//String NAMED = String("Lime");
+//String NAMED = String("Lemon");
+//String NAMED = String("Orange");
+//String NAMED = String("Grass");
+//String NAMED = String("Apple");
 
 #include <databot.h>
 
 void setName() {
 
-  String nameCmd = F("AT+NAMEdatabot");
+  String nameCmd = F("AT+NAMEDB_");
 
   nameCmd += NAMED;
 
